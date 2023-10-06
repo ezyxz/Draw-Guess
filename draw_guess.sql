be925f7cdd55234352000ee505d920b94b3ab742
ezyxz <1141148686@qq.com>
2023-10-06 09:17:37 +0800

    add base sql statement

---
+++ draw_guess.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Enterprise v12.09 (64 bit)
+MySQL - 5.5.40 : Database - draw_and_guess
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`draw_and_guess` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `draw_and_guess`;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `pwd` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`name`,`pwd`) values (1,'admin','admin'),(2,'jack','123'),(3,'ezyxz','111'),(4,'jack','123'),(5,'ezyxz','123'),(6,'tom','123'),(7,'ie','123'),(8,'MXS','123'),(9,'钟宇峰','123'),(10,'zyf','123'),(11,'fky','123'),(12,'a','a'),(13,'qwe','123'),(14,'lei','lei');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
